1c5327f2dad6c0f1f1097d9389f699358d27e3fc
Mhnd <mrmohandtv@gmail.com>
2020-08-18 09:08:49 +0300

    adding image

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const VideoDetail = ({ video }) => {
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
       <div className="ui embed">
